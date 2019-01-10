6d3dcb4cdcfde3c8772aeebec0b98a66aa9f3cdc
user2 <user2@user2-PC>
2019-01-10 20:09:12 +0900

    이건충돌될꺼야!

--- gitTest/src/git/test/Main.java
+++ gitTest/src/git/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("추가한내용");
+		int age=19;
 		System.out.println("두번째 수정");
 		System.out.println("이거충돌나야돼!");
 	}
